5f023068e00a934910ddda511190ab154c66685a
Md Minhazul Abedin <30324909+bappy451@users.noreply.github.com>
2023-11-14 13:00:41 +0600

    Add files via upload

---
+++ src/main/java/com/reddot/javatracing/JavaTracingApplication.java
@@ -0,0 +1,13 @@
+package com.reddot.javatracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTracingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTracingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reddot/javatracing/JavaTracingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reddot.javatracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTracingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
